e1c3b1845b5eea5493912190a7b3db66fdc4ec08
Enrico Pais <58297846+BadBoros@users.noreply.github.com>
2020-05-04 11:04:58 +0200

    Update book-services.php

--- book-services.php
+++ book-services.php
@@ -48,7 +48,7 @@ echo "<script>window.location.href ='services.php'</script>";
 <!DOCTYPE html>
 <html>
 <head>
-<title>Online DJ Management System || Contact Us</title>
+<title>DjBooking || Contact Us</title>
 <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
 <!-- Custom Theme files -->
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
